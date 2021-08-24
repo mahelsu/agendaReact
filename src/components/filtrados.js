0d9b26569de58ae0f0e9cac90647060127c3db61
Huascar Mejia <mejiahuascar@gmail.com>
2021-08-23 20:46:39 -0400

    Definitive definitive commit

---
+++ src/components/filtrados.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Table, Button} from "reactstrap";
+
+const filtrados = ({data, eliminar, editar}) =>{
+    return(
+        <Table>
+            <thead><tr><th>ID</th>
+            <th>Nombre</th>
+            <th>Numero Telefonico</th>
+            <th>Acciones</th></tr></thead>
+            <tbody>
+            {data.map((elemento)=>(
+                <tr>
+                    <td>{elemento.id}</td>
+                    <td>{elemento.nombre}</td>
+                    <td>{elemento.telefono}</td>
+                    <td><Button color="primary" onClick={() => editar(elemento)}>Editar</Button></td>
+                    <td><Button color="danger" onClick={() => eliminar(elemento)}>Eliminar</Button></td>
+                </tr>               
+            ))}
+            </tbody>
+        </Table> 
+    )
+}
+export default filtrados;
+      
